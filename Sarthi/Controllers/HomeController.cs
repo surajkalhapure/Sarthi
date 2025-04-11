cac51da732ad728f39d96e4aba38fbcf4c20f822
surajkalhapure <surajkalhapure143@gmail.com>
2025-04-12 00:20:59 +0530

    ok

--- Sarthi/Controllers/HomeController.cs
+++ Sarthi/Controllers/HomeController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sarthi.Models;
 
 namespace Sarthi.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        SarthiContext Sa=new SarthiContext();
         public ActionResult Index()
         {
             
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+
+
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+
+
+
             return View();
         }
     }
---
+++ Sarthi/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sarthi.Models
+{
+    public class Register
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DOB { get; set; }
+        public string Pass {  get; set; }
+
+    }
+}
